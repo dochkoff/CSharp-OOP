d7b7b3b2ff7519d51f3eac8972439403fd69e7b9
dochkoff <hounds.07cinder@icloud.com>
2023-02-23 22:35:52 +0200

    T03-P05

---
+++ C# OOP - 2023/T03_Inheritance/P05_StackOfStrings/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            StackOfStrings customStack = new();
+            Console.WriteLine(customStack.IsEmpty());
+
+            List<string> list = new() { "A", "B", "C", "D", "F" };
+            customStack.AddRange(list);
+            Console.WriteLine(customStack.IsEmpty());
+
+            foreach (var item in customStack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
