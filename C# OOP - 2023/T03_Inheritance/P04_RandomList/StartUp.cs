3727ae724b5275d93d418c078ede9721309534b9
dochkoff <hounds.07cinder@icloud.com>
2023-02-23 22:02:26 +0200

    T03-P04

---
+++ C# OOP - 2023/T03_Inheritance/P04_RandomList/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            RandomList list = new() { "A", "B", "C", "D", "F" };
+
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
