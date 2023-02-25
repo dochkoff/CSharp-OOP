8fb6401a9266d86198680c13f8b7126b8048a209
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-25 20:55:05 +0200

    T04-P04

---
+++ C# OOP - 2023/T04_Inheritance_Exercise/NeedForSpeed/FamilyCar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace NeedForSpeed
+{
+    public class FamilyCar : Car
+    {
+        public FamilyCar(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+        }
+    }
+}
+
---
+++ C# OOP - 2023/T04_Inheritance_Exercise/NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        private const double DefaultFuelConsumption = 8;
+
+        public RaceMotorcycle(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+        }
+
+        public override double FuelConsumption => DefaultFuelConsumption;
+    }
+}
+
