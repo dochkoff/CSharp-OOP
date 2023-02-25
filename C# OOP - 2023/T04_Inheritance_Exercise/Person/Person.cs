5a6f095b9d53e03d2515b641ef30395d0f85b7da
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-25 18:48:31 +0200

    T04_P01

---
+++ C# OOP - 2023/T04_Inheritance_Exercise/Person/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public virtual int Age
+        {
+            get => age;
+            set
+            {
+                if (value > 0)
+                {
+                    age = value;
+                }
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
+
