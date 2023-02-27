7fa48608e4661c3d2db5f795ca4c91ad2d12f155
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-27 20:51:37 +0200

    T05-P02

---
+++ C# OOP - 2023/T05_Encapsulation/P02_Salary/Person.cs
@@ -0,0 +1,62 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            private set { firstName = value; }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            private set { lastName = value; }
+        }
+
+        private int age;
+
+        public int Age
+        {
+            get { return age; }
+            private set { age = value; }
+        }
+
+        private decimal salary;
+
+        public decimal Salary
+        {
+            get { return salary; }
+            private set { salary = value; }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} recaives {Salary:F2} leva.";
+        }
+    }
+}
