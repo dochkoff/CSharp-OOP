e3fd8bb63595f1aade82bf57c488bbb6e8365e26
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-02-27 21:20:18 +0200

    T05-P04

---
+++ C# OOP - 2023/T05_Encapsulation/P04_FirstAndReserveTeam/Team.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using PersonsInfo;
+
+namespace P04_FirstAndReserveTeam
+{
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam = new();
+            this.reserveTeam = new();
+        }
+
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get => this.firstTeam.AsReadOnly();
+        }
+
+        public IReadOnlyCollection<Person> ReserveTeam
+        {
+            get => this.reserveTeam.AsReadOnly();
+        }
+
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+    }
+}
