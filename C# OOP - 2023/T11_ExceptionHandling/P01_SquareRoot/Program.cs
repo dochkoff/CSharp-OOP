66cd1c87d5cad17a97e5af204048143d3ba38dd8
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-03-13 19:15:53 +0200

    T11-P01

---
+++ C# OOP - 2023/T11_ExceptionHandling/P01_SquareRoot/Program.cs
@@ -0,0 +1,21 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+try
+{
+    if (number >= 0)
+    {
+        Console.WriteLine(Math.Sqrt(number));
+    }
+    else
+    {
+        throw new ArgumentException("Invalid number.");
+    }
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Goodbye.");
+}
