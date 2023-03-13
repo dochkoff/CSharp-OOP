bf5d49b217e6fcfec23d10cc77dc1d5cfc3e6d64
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-03-13 23:06:00 +0200

    T11-P02

---
+++ C# OOP - 2023/T11_ExceptionHandling/P02_EnterNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿public class StartUp
+{
+    static void Main()
+    {
+        Console.WriteLine(ReadNumber(1, 100));
+    }
+
+    static string ReadNumber(int start, int end)
+    {
+        int lastAddedNum = 1;
+        List<int> enteredNums = new();
+
+        while (enteredNums.Count < 10)
+        {
+            string input = Console.ReadLine();
+
+            try
+            {
+                if (int.TryParse(input, out int num))
+                {
+                    if (1 < num && num < 100)
+                    {
+                        enteredNums.Add(num);
+                        lastAddedNum = num;
+                    }
+                    else
+                    {
+                        throw new ArgumentException($"Your number is not in range {lastAddedNum} - 100!");
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid Number!");
+                }
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        return string.Join(", ", enteredNums);
+    }
+}
