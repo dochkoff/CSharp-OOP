7f6465c0772e7f0965218bce9b92ca89225addce
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-03-14 22:13:25 +0200

    T11-P04

---
+++ C# OOP - 2023/T11_ExceptionHandling/P04_SumOfIntegers/Program.cs
@@ -0,0 +1,25 @@
+﻿int sum = 0;
+
+string[] input = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+foreach (var element in input)
+{
+    try
+    {
+        int nun = int.Parse(element);
+        sum += nun;
+
+    }
+    catch (OverflowException)
+    {
+        Console.WriteLine($"The element '{element}' is out of range!");
+    }
+    catch (FormatException fe)
+    {
+        Console.WriteLine($"The element '{element}' is in wrong format!");
+    }
+    Console.WriteLine($"Element '{element}' processed - current sum: {sum}");
+}
+
+Console.WriteLine($"The total sum of all integers is: {sum}");
