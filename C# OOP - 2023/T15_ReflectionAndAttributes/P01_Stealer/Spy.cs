309a44f1dd2a36f0f2dbb2f0dbb47286ac754329
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-03-22 22:53:27 +0200

    T15-P01

---
+++ C# OOP - 2023/T15_ReflectionAndAttributes/P01_Stealer/Spy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string target, params string[] targetInfo)
+        {
+            Type classType = Type.GetType(target);
+            FieldInfo[] classFields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+            StringBuilder sb = new();
+
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {target}");
+
+            foreach (FieldInfo field in classFields.Where(f => targetInfo.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
+
---
+++ C# OOP - 2023/T15_ReflectionAndAttributes/P01_Stealer/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using Stealer;
+
+public class StartUp
+{
+    static void Main()
+    {
+        Spy spy = new();
+        string result = spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+        Console.WriteLine(result);
+    }
+}
