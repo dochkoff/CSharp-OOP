6f605e9b52b01049db2df9545da4e72711506ed2
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-03-24 23:41:29 +0200

    T15-P06

---
+++ C# OOP - 2023/T15_ReflectionAndAttributes/P06_CodeTracker/Tracker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+
+        public void PrintMethodsByAuthor()
+        {
+            Type classType = typeof(StartUp);
+            MethodInfo[] methodInfos = classType.GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+
+            foreach (var method in methodInfos)
+            {
+                if (method.CustomAttributes.Any(m => m.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = method.GetCustomAttributes(false);
+
+                    foreach (AuthorAttribute attr in attributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attr.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
+
