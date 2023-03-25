3e526de08296e82c3e07bbbd6c38423f1b0722e7
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-03-25 23:09:01 +0200

    T17-P02

--- C# OOP - 2023/T17_UnitTesting/Skeleton.Tests/DummyTests.cs
+++ C# OOP - 2023/T17_UnitTesting/Skeleton.Tests/DummyTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace Skeleton.Tests
 {
@@ -8,7 +9,39 @@ namespace Skeleton.Tests
         [Test]
         public void DummnyLosesHealthIfAttacked()
         {
+            Dummy target = new(10, 10);
 
+            target.TakeAttack(5);
+
+            Assert.That(target.Health, Is.EqualTo(5), "Dummy doesn't lose health if attacked.");
+        }
+
+        [Test]
+        public void DeadDummyThrowsAnExceptionIfAttacked()
+        {
+            Dummy target = new(4, 10);
+
+            target.TakeAttack(5);
+
+            Assert.Throws<InvalidOperationException>(() => target.TakeAttack(5), "Dead Dummy doesn't throw an exception if attacked.");
+        }
+
+        [Test]
+        public void DeadDummyCanGiveXP()
+        {
+            Dummy target = new(0, 10);
+
+            int exp = target.GiveExperience();
+
+            Assert.That(exp, Is.EqualTo(10), "Dead Dummy can't give XP.");
+        }
+
+        [Test]
+        public void AliveDummyCantGiveXP()
+        {
+            Dummy target = new(10, 10);
+
+            Assert.Throws<InvalidOperationException>(() => target.GiveExperience(), "Alive Dummy can give XP.");
         }
     }
 }
