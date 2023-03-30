f7edf29b0470d3c958584301649cc0cc55869a95
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-03-30 11:51:38 +0300

    T18-P04

--- C# OOP - 2023/T18_UnitTesting_Exercise/Exercise/FightingArena.Tests/ArenaTests.cs
+++ C# OOP - 2023/T18_UnitTesting_Exercise/Exercise/FightingArena.Tests/ArenaTests.cs
@@ -1,10 +1,99 @@
 ﻿namespace FightingArena.Tests
 {
+    using System;
+    using System.Linq;
+    using System.Threading;
     using NUnit.Framework;
 
     [TestFixture]
     public class ArenaTests
     {
+        Arena arena;
+        Warrior warrior1;
+        Warrior warrior2;
+        Warrior warrior3;
 
+        [SetUp]
+        public void SetUp()
+        {
+            arena = new Arena();
+            warrior1 = new Warrior("Richi", 10, 50);
+            warrior2 = new Warrior("Kuchi", 15, 40);
+            warrior3 = new Warrior("Richi", 20, 30);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            arena = null;
+            warrior1 = null;
+            warrior2 = null;
+            warrior3 = null;
+        }
+
+        [Test]
+        public void EnrollWarrior()
+        {
+            arena.Enroll(warrior1);
+
+            Assert.That(arena.Warriors.Any(w => w.Name == warrior1.Name));
+        }
+
+        [Test]
+        public void EnrollWarriorWithSameName()
+        {
+            arena.Enroll(warrior1);
+
+            InvalidOperationException exception =
+                Assert.Throws<InvalidOperationException>(() => arena.Enroll(warrior3));
+            Assert.That(exception.Message, Is.EqualTo("Warrior is already enrolled for the fights!"));
+        }
+
+        [Test]
+        public void CountOfEnroledWarriors()
+        {
+            arena.Enroll(warrior1);
+            Assert.That(arena.Count, Is.EqualTo(1));
+            Assert.That(arena.Count, Is.EqualTo(arena.Warriors.Count));
+        }
+
+        [Test]
+        public void FightMethod()
+        {
+            string attackerName = "Richi";
+            string defenderName = "Kuchi";
+            arena.Enroll(warrior1);
+            arena.Enroll(warrior2);
+            arena.Fight(attackerName, defenderName);
+
+            Assert.That(warrior1.HP, Is.EqualTo(35));
+            Assert.That(warrior2.HP, Is.EqualTo(30));
+        }
+
+        [Test]
+        public void FightWithNoSuchAttakerName()
+        {
+            string attackerName = "Richard";
+            string defenderName = "Kuchi";
+            arena.Enroll(warrior1);
+            arena.Enroll(warrior2);
+
+            InvalidOperationException exception =
+                Assert.Throws<InvalidOperationException>(() => arena.Fight(attackerName, defenderName));
+            Assert.That(exception.Message, Is.EqualTo($"There is no fighter with name {attackerName} enrolled for the fights!"));
+        }
+
+        [Test]
+        public void FightWithNoSuchDefenderName()
+        {
+            string attackerName = "Richi";
+            string defenderName = "Kuchard";
+            arena.Enroll(warrior1);
+            arena.Enroll(warrior2);
+
+            InvalidOperationException exception =
+                Assert.Throws<InvalidOperationException>(() => arena.Fight(attackerName, defenderName));
+            Assert.That(exception.Message, Is.EqualTo($"There is no fighter with name {defenderName} enrolled for the fights!"));
+        }
     }
 }
--- C# OOP - 2023/T18_UnitTesting_Exercise/Exercise/FightingArena.Tests/WarriorTests.cs
+++ C# OOP - 2023/T18_UnitTesting_Exercise/Exercise/FightingArena.Tests/WarriorTests.cs
@@ -1,10 +1,114 @@
 namespace FightingArena.Tests
 {
+    using System;
     using NUnit.Framework;
 
     [TestFixture]
     public class WarriorTests
     {
+        Warrior warrior;
+        private const int MinAttackHP = 30;
 
+        [SetUp]
+        public void SetUp()
+        {
+            warrior = new Warrior("Richi", 10, 50);
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            warrior = null;
+        }
+
+        [Test]
+        public void CreateWarrior()
+        {
+            Assert.That(warrior.Name, Is.EqualTo("Richi"));
+            Assert.That(warrior.Damage, Is.EqualTo(10));
+            Assert.That(warrior.HP, Is.EqualTo(50));
+        }
+
+        [Test]
+        public void SetNullOrEmptyWarriorName()
+        {
+            ArgumentException exception1 =
+                Assert.Throws<ArgumentException>(() => warrior = new Warrior(null, 10, 50));
+            Assert.That(exception1.Message, Is.EqualTo("Name should not be empty or whitespace!"));
+
+            ArgumentException exception2 =
+                Assert.Throws<ArgumentException>(() => warrior = new Warrior(string.Empty, 10, 50));
+            Assert.That(exception2.Message, Is.EqualTo("Name should not be empty or whitespace!"));
+        }
+
+        [Test]
+        public void SetNegativeOrZeroWarriorDamage()
+        {
+            ArgumentException exception =
+                Assert.Throws<ArgumentException>(() => warrior = new Warrior("Richi", 0, 50));
+            Assert.That(exception.Message, Is.EqualTo("Damage value should be positive!"));
+        }
+
+        [Test]
+        public void SetNegativeWarriorHP()
+        {
+            ArgumentException exception =
+                Assert.Throws<ArgumentException>(() => warrior = new Warrior("Richi", 10, -1));
+            Assert.That(exception.Message, Is.EqualTo("HP should not be negative!"));
+        }
+
+        [Test]
+        public void AttackWhenTargetWarriorIsWithBiggerHPThanWarriorDamage()
+        {
+            Warrior targetWarrior = new("Kuchi", 5, 60);
+            warrior.Attack(targetWarrior);
+
+            Assert.That(warrior.HP, Is.EqualTo(45));
+            Assert.That(targetWarrior.HP, Is.EqualTo(50));
+        }
+
+        [Test]
+        public void AttackWhenTargetWarriorIsWithSmallerHPThanWarriorDamage()
+        {
+            Warrior warrior = new("Richi", 40, 50);
+            Warrior targetWarrior = new("Kuchi", 5, 35);
+            warrior.Attack(targetWarrior);
+
+            Assert.That(targetWarrior.HP, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void AttackWhenHPIsBelowTheMinimum()
+        {
+            Warrior warrior = new("Richi", 10, 20);
+            Warrior targetWarrior = new("Kuchi", 5, 60);
+
+
+            InvalidOperationException exception =
+                Assert.Throws<InvalidOperationException>(() => warrior.Attack(targetWarrior));
+            Assert.That(exception.Message, Is.EqualTo("Your HP is too low in order to attack other warriors!"));
+        }
+
+        [Test]
+        public void AttackWhenTargetWariorHPIsBelowTheMinimum()
+        {
+            Warrior targetWarrior = new("Kuchi", 5, 20);
+
+
+            InvalidOperationException exception =
+                Assert.Throws<InvalidOperationException>(() => warrior.Attack(targetWarrior));
+            Assert.That(exception.Message, Is.EqualTo($"Enemy HP must be greater than {MinAttackHP} in order to attack him!"));
+        }
+
+        [Test]
+        public void AttackWhenTryToAttackStrongerEnemy()
+        {
+            Warrior targetWarrior = new("Kuchi", 60, 60);
+
+            InvalidOperationException exception =
+                Assert.Throws<InvalidOperationException>(() => warrior.Attack(targetWarrior));
+            Assert.That(exception.Message, Is.EqualTo("You are trying to attack too strong enemy"));
+        }
     }
 }
