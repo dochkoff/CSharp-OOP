75c81bbb57f6ebada0bef72c7cd22ed80ea1338d
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-04-02 18:23:17 +0300

    T22-P01

---
+++ C# OOP - 2023/T22_DesignPatterns_Exercise/P01_Prototype/Models/SandwichMenu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace P01_Prototype.Models
+{
+    public class SandwichMenu
+    {
+        private Dictionary<string, SandwichPrototype> sandwiches = new();
+
+        public SandwichPrototype this[string name]
+        {
+            get { return sandwiches[name]; }
+            set { sandwiches.Add(name, value); }
+        }
+    }
+}
---
+++ C# OOP - 2023/T22_DesignPatterns_Exercise/P01_Prototype/Models/SandwichPrototype.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace P01_Prototype.Models
+{
+    public abstract class SandwichPrototype
+    {
+        public abstract SandwichPrototype Clone();
+    }
+}
