08b6cb63aa418e5c58afa8baa74a9cfc75056c71
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-04-07 18:01:19 +0300

    T23-P02

--- C# OOP - 2023/T23_ExamPreparation/P01&P02_UniversityCompetition/Core/Controller.cs
+++ C# OOP - 2023/T23_ExamPreparation/P01&P02_UniversityCompetition/Core/Controller.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Xml.Linq;
 using UniversityCompetition.Core.Contracts;
 using UniversityCompetition.Models;
 using UniversityCompetition.Models.Contracts;
@@ -12,7 +14,6 @@ namespace UniversityCompetition.Core
 {
     public class Controller : IController
     {
-        //FIXME
         private IRepository<ISubject> subjects = new SubjectRepository();
         private IRepository<IStudent> students = new StudentRepository();
         private IRepository<IUniversity> universities = new UniversityRepository();
@@ -21,10 +22,6 @@ namespace UniversityCompetition.Core
         {
         }
 
-        public string AddStudent(string firstName, string lastName)
-        {
-            throw new NotImplementedException();
-        }
 
         public string AddSubject(string subjectName, string subjectType)
         {
@@ -34,51 +31,158 @@ namespace UniversityCompetition.Core
                 return string.Format(OutputMessages.AlreadyAddedSubject, subjectName);
             }
 
+            ISubject subject;
             if (subjectType == "TechnicalSubject")
             {
-                TechnicalSubject technicalSubject = new(1, subjectName);
-                subjects.AddModel(technicalSubject);
-
-                return string.Format(OutputMessages.SubjectAddedSuccessfully, subjectType, subjectName, subjects);
+                subject = new TechnicalSubject(1, subjectName);
             }
             else if (subjectType == "EconomicalSubject")
             {
-                EconomicalSubject economicalSubject = new(1, subjectName);
-                subjects.AddModel(economicalSubject);
-
-                return string.Format(OutputMessages.SubjectAddedSuccessfully, subjectType, subjectName, subjects);
+                subject = new EconomicalSubject(1, subjectName);
             }
             else if (subjectType == "HumanitySubject")
             {
-                HumanitySubject humanitySubject = new(1, subjectName);
-                subjects.AddModel(humanitySubject);
-
-                return string.Format(OutputMessages.SubjectAddedSuccessfully, subjectType, subjectName, subjects);
+                subject = new HumanitySubject(1, subjectName);
             }
             else
             {
                 return string.Format(OutputMessages.SubjectTypeNotSupported, subjectType);
             }
+
+            subjects.AddModel(subject);
+
+            return string.Format(OutputMessages.SubjectAddedSuccessfully, subjectType, subjectName, subjects.GetType().Name);
         }
+
+
+
 
         public string AddUniversity(string universityName, string category, int capacity, List<string> requiredSubjects)
         {
-            throw new NotImplementedException();
+            if (universities.FindByName(universityName) != null)
+            {
+                return string.Format(OutputMessages.AlreadyAddedUniversity, universityName);
+            }
+
+            List<int> requiredSubjectsToInt = new();
+
+            foreach (var reqSub in requiredSubjects)
+            {
+                ISubject subject = subjects.FindByName(reqSub);
+                requiredSubjectsToInt.Add(subject.Id);
+            }
+
+            IUniversity university = new University(1, universityName, category, capacity, requiredSubjectsToInt);
+
+            universities.AddModel(university);
+
+            return string.Format(OutputMessages.UniversityAddedSuccessfully, universityName, universities.GetType().Name);
         }
 
-        public string ApplyToUniversity(string studentName, string universityName)
+
+
+
+        public string AddStudent(string firstName, string lastName)
         {
-            throw new NotImplementedException();
+            string fullName = firstName + " " + lastName;
+            if (students.FindByName(fullName) != null)
+            {
+                return string.Format(OutputMessages.AlreadyAddedStudent, firstName, lastName);
+            }
+
+            IStudent student = new Student(1, firstName, lastName);
+            students.AddModel(student);
+
+            return string.Format(OutputMessages.StudentAddedSuccessfully, firstName, lastName, students.GetType().Name);
         }
+
+
 
         public string TakeExam(int studentId, int subjectId)
         {
-            throw new NotImplementedException();
+            IStudent student = students.FindById(studentId);
+            ISubject subject = subjects.FindById(subjectId);
+
+            if (student == null)
+            {
+                return OutputMessages.InvalidStudentId;
+            }
+
+            if (student == null)
+            {
+                return OutputMessages.InvalidSubjectId;
+            }
+
+            if (student.CoveredExams.Contains(subjectId))
+            {
+                return string.Format(OutputMessages.StudentAlreadyCoveredThatExam, student.FirstName, student.LastName, subject.Name);
+
+            }
+
+            student.CoverExam(subject);
+
+            return string.Format(OutputMessages.StudentSuccessfullyCoveredExam, student.FirstName, student.LastName, subject.Name);
         }
+
+
+
+        public string ApplyToUniversity(string studentName, string universityName)
+        {
+            IStudent student = students.FindByName(studentName);
+            IUniversity university = universities.FindByName(universityName);
+
+            if (student == null)
+            {
+                return string.Format(OutputMessages.StudentNotRegitered, student.FirstName, student.LastName);
+            }
+
+            if (university == null)
+            {
+                return string.Format(OutputMessages.UniversityNotRegitered, universityName);
+            }
+
+            foreach (var requiredExam in university.RequiredSubjects)
+            {
+                if (!student.CoveredExams.Contains(requiredExam))
+                {
+                    return String.Format(OutputMessages.StudentHasToCoverExams, studentName, universityName);
+                }
+            }
+
+            if (student.University != null && student.University.Name == universityName)
+            {
+                return string.Format(OutputMessages.StudentAlreadyJoined, student.FirstName, student.LastName, universityName);
+            }
+
+            student.JoinUniversity(university);
+
+            return string.Format(OutputMessages.StudentSuccessfullyJoined, student.FirstName, student.LastName, universityName);
+        }
+
+
 
         public string UniversityReport(int universityId)
         {
-            throw new NotImplementedException();
+            IUniversity university = universities.FindById(universityId);
+            int studentsCount = 0;
+
+            foreach (var student in students.Models)
+            {
+                if (student.University?.Id == university.Id)
+                {
+                    studentsCount++;
+                }
+            }
+
+            int capacityLeft = university.Capacity - studentsCount;
+            StringBuilder sb = new();
+
+            sb.AppendLine($"*** {university.Name} ***");
+            sb.AppendLine($"Profile: {university.Category}");
+            sb.AppendLine($"Students admitted: {studentsCount}");
+            sb.AppendLine($"University vacancy: {capacityLeft}");
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
