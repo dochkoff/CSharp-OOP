795ed256727967f3086f54b59f5e060dfb6851ef
Pavel Dochkov <hounds.07cinder@icloud.com>
2023-04-10 23:22:55 +0300

    Update UnitTest1.cs

--- C# OOP - 2023/T27_RegularExam/P03_RobotFactory/RobotFactory.Tests/UnitTest1.cs
+++ C# OOP - 2023/T27_RegularExam/P03_RobotFactory/RobotFactory.Tests/UnitTest1.cs
@@ -1,5 +1,7 @@
 using NUnit.Framework;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 
 namespace RobotFactory.Tests
 {
@@ -9,14 +11,14 @@ namespace RobotFactory.Tests
         [SetUp]
         public void Setup()
         {
-            factory = new Factory("Gosho", 10);
+            factory = new Factory("Testla", 10);
         }
 
         [Test]
         public void CtroShouldWork()
         {
-            Factory factory = new Factory("Gosho", 10);
-            Assert.AreEqual("Gosho", factory.Name);
+            Factory factory = new Factory("Testla", 10);
+            Assert.AreEqual("Testla", factory.Name);
             Assert.AreEqual(10, factory.Capacity);
             Assert.IsNotNull(factory.Robots);
             Assert.IsNotNull(factory.Supplements);
@@ -25,19 +27,19 @@ namespace RobotFactory.Tests
         [Test]
         public void ProduceRobotShoulThrow()
         {
-            Factory factory = new Factory("Gosho", 0);
-            //Robot robot = new Robot("1gfg", 10.0, 10);
-            //factory.ProduceRobot("1gfg", 10.0, 10);
+            Factory factory = new Factory("Testla", 0);
+            //Robot robot = new Robot("X", 10.0, 10);
+            //factory.ProduceRobot("X", 10.0, 10);
 
-            Assert.AreEqual("The factory is unable to produce more robots for this production day!", factory.ProduceRobot("1gfg", 10.0, 10));
+            Assert.AreEqual("The factory is unable to produce more robots for this production day!", factory.ProduceRobot("X", 10.0, 10));
             Assert.AreEqual(0, factory.Robots.Count);
         }
 
         [Test]
         public void ProduceRobotShouldWork()
         {
-            Factory factory = new Factory("Gosho", 10);
-            Assert.AreEqual($"Produced --> Robot model: 1gfg IS: 10, Price: 10,00", factory.ProduceRobot("1gfg", 10.0, 10));
+            Factory factory = new Factory("Testla", 10);
+            Assert.AreEqual($"Produced --> Robot model: X IS: 10, Price: 10.00", factory.ProduceRobot("X", 10.0, 10));
             Assert.AreEqual(1, factory.Robots.Count);
         }
 
@@ -45,17 +47,17 @@ namespace RobotFactory.Tests
         public void ProduceSupplShouldWork()
         {
 
-            Assert.AreEqual("Supplement: Bira IS: 10", factory.ProduceSupplement("Bira", 10));
+            Assert.AreEqual("Supplement: falconWings IS: 10", factory.ProduceSupplement("falconWings", 10));
             Assert.AreEqual(1, factory.Supplements.Count);
         }
 
         [Test]
         public void UpgradeShouldReturnFalse()
         {
-            Robot robot = new Robot("1gfg", 10.0, 10);
-            Supplement supl = new Supplement("Bira", 9);
-            factory.ProduceRobot("1gfg", 10.0, 10);
-            factory.ProduceSupplement("Bira", 10);
+            Robot robot = new Robot("X", 10.0, 10);
+            Supplement supl = new Supplement("falconWings", 9);
+            factory.ProduceRobot("X", 10.0, 10);
+            factory.ProduceSupplement("falconWings", 10);
 
             Assert.IsFalse(factory.UpgradeRobot(robot, supl));
         }
@@ -63,10 +65,10 @@ namespace RobotFactory.Tests
         [Test]
         public void UpgradeShouldReturnTrue()
         {
-            Robot robot = new Robot("1gfg", 10.0, 10);
-            Supplement supl = new Supplement("Bira", 10);
-            factory.ProduceRobot("1gfg", 10.0, 10);
-            factory.ProduceSupplement("Bira", 10);
+            Robot robot = new Robot("X", 10.0, 10);
+            Supplement supl = new Supplement("falconWings", 10);
+            factory.ProduceRobot("X", 10.0, 10);
+            factory.ProduceSupplement("falconWings", 10);
 
             Assert.IsTrue(factory.UpgradeRobot(robot, supl));
             Assert.AreEqual(1, robot.Supplements.Count);
@@ -77,12 +79,13 @@ namespace RobotFactory.Tests
         {
 
             Robot robot1 = new Robot("1gf", 12.0, 11);
-            factory.ProduceRobot("1gfg", 10.0, 10);
+            factory.ProduceRobot("X", 10.0, 10);
             factory.ProduceRobot("1gf", 12.0, 11);
-            Robot robot = factory.Robots.FirstOrDefault(r => r.Model == "1gfg");
+            Robot robot = factory.Robots.FirstOrDefault(r => r.Model == "X");
             var expectedOut = factory.SellRobot(10);
             Assert.That(expectedOut, Is.EqualTo(robot));
 
         }
+
     }
 }
